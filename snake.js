bd51b00608a70679c92cc878b184ef6ef3304a74
Warsame Ahmed <wahmed04@dcs.bbk.ac.uk>
2020-02-09 15:32:32 +0000

    Snake now has a tail which grows after everytime its consumes

--- snake.js
+++ snake.js
@@ -5,19 +5,29 @@ class Snake{
       this.x = x;
       this.y = y;
       this.color = color;
-  
+      this.total = 1;
+      this.tail = [];
     }
 
     draw(){
       
       ctx.fillStyle = this.color;
+      for (let i = 0; i < this.tail.length -1; i++ ){
+        ctx.fillRect(this.tail[i].x, this.tail[i].y, this.width, this.height);
+      }
       ctx.fillRect(this.x, this.y, this.width, this.height); 
     }
   
     update(){
       
-      ctx.fillStyle = this.color;
-      ctx.fillRect(this.x, this.y, this.width, this.height); 
+      for (let i = 0; i < this.tail.length -1; i++ ){
+        this.tail[i] = this.tail[i+1];
+      }
+
+      this.tail[this.total -1]= {x : this.x, y : this.y};
+
+      this.draw();
+       
       
       if (this.x > canvas.width){
         this.x = 0;
@@ -35,6 +45,7 @@ class Snake{
 
     consume(food){
       if (this.x === food.x && this.y === food.y){
+        this.total++;
         return true;
       }
       return false;
