978914ea74e10b02ba78b8e6d18ebf111cdedef0
Warsame Ahmed <wahmed04@dcs.bbk.ac.uk>
2020-02-13 00:10:50 +0000

    Removed the 'CrashWith' function from component

--- index.js
+++ index.js
@@ -112,25 +112,6 @@ function startGame() {
           }
       }
 
-      this.crashWith = function(otherobj) {
-        var myleft = this.x;
-        var myright = this.x + (this.width);
-        var mytop = this.y;
-        var mybottom = this.y + (this.height);
-        var otherleft = otherobj.x;
-        var otherright = otherobj.x + (otherobj.width);
-        var othertop = otherobj.y;
-        var otherbottom = otherobj.y + (otherobj.height);
-        var crash = true;
-        if ((mybottom < othertop) ||
-        (mytop > otherbottom) ||
-        (myright < otherleft) ||
-        (myleft > otherright)) {
-          crash = false;
-        }
-        return crash;
-      }
-
   }
 
 
